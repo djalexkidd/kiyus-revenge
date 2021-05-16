f8b4c17f0e671fbaa2f5911e2d890f0876275a2c
djalexkidd <alexpyr@outlook.fr>
2021-05-16 17:57:11 -0400

    Ajustement du volume sonore Amélioration du script de la brique

--- scripts/props/brick.gd
+++ scripts/props/brick.gd
@@ -1,10 +1,15 @@
 extends StaticBody2D
 
-#La fameuse âme des Toads du royaume champignon, transformée en brique par le méchant Bowser!
 #Comme pour le bloc mystère, celle-ci se casse lorsque le joueur frappe par dessous.
 
-#TODO : Implémenter une animation de casse et un son
-#J'ai pas fini !
+#TODO : Implémenter une animation de casse
 
 func _on_bottom_checker_body_entered(body):
-	queue_free() #Le bloc disparait de la RAM lorsque le joueur frappe le bloc
+	set_collision_layer_bit(1,false)
+	set_collision_mask_bit(0,false)
+	$bottom_checker.set_collision_mask_bit(0,false)
+	hide()
+	$BreakSound.play()
+
+func _on_BreakSound_finished():
+	queue_free() #Le bloc disparait de la RAM lorsque le son se termine.
