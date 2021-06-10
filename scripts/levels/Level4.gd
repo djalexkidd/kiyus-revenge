f5cb720e9e612f4210a3ba619e9c118614fc5df4
djalexkidd <alexpyr@outlook.fr>
2021-06-10 22:14:11 +0200

    Pont se pète procéduralement

--- scripts/levels/Level4.gd
+++ scripts/levels/Level4.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
+var x = 137
+
 func _ready():
 	if Global.pipe_number == 4:
 		Global.coins = 0
 		$HUD._ready()
 
 func _on_KillSwitch_pressed(): #Le niveau reçoit le signal de l'interrupteur
-	$Bridge.queue_free() #Le pont se détruit de la RAM
+	$Bridge/Timer.start()
+	#$Bridge.queue_free() #Le pont se détruit de la RAM
 
 func _on_boss_trigger_body_entered(body):
 	$enemies/Paulok/LanceKiyu.start()
@@ -14,3 +17,9 @@ func _on_boss_trigger_body_entered(body):
 
 func _on_PeppaPig_finished():
 	$boss_trigger.queue_free()
+
+func _on_aaaaa_timeout():
+	for i in range(x,152):
+		x = x + 1
+		$Bridge.set_cell(x,6,-1)
+		break
