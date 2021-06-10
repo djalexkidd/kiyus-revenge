67e9cded6ed1b833adf943471ec7cfecc08fd86f
djalexkidd <alexpyr@outlook.fr>
2021-06-10 17:23:22 +0200

    Ajout d'un Timer pour finir le niveau

--- scripts/other/HUD.gd
+++ scripts/other/HUD.gd
@@ -9,6 +9,10 @@ func _on_coin_collected(): #Ajoute une piece au compteur
 	Global.coins = Global.coins + 1
 	_ready()
 
-func _physics_process(delta): #Compteur de FPS
-	if Global.fps_counter:
+func _physics_process(delta):
+	$Time.text = "%d:%02d" % [floor($Timer.time_left / 60), int($Timer.time_left) % 60] #Affiche le Timer
+	if Global.fps_counter: #Compteur de FPS activé
 		$FPScounter.text = String(Engine.get_frames_per_second()) #Actualise le compteur de FPS
+
+func _on_Timer_timeout(): #Si le Timer est écoulé
+	get_tree().change_scene("res://scenes/menu/GameOver.tscn") #Le joueur pert
