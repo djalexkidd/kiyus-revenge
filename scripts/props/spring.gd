98ed3b4d9dcaedc9cca747be9b826210c741cb2e
djalexkidd <alexpyr@outlook.fr>
2021-06-11 16:17:16 +0200

    SQUEEZIE

--- scripts/props/spring.gd
+++ scripts/props/spring.gd
@@ -5,6 +5,10 @@ func _on_spring_body_entered(body):
 	$AnimatedSprite.play("ON")
 	$spring_sound.play()
 	$Timer.start()
+	$squeeze.start()
 
 func _on_Timer_timeout():
 	$AnimatedSprite.play("OFF")
+
+func _on_squeeze_timeout():
+	$kiyu_spring.play()
