1bb5ce71d30270cd7be7fc1dae75a4521ed10ac6
djalexkidd <alexpyr@outlook.fr>
2021-06-11 14:15:31 +0200

    Ajout ressort Ajustement Timer Placeholder Niveau 5

---
+++ scripts/props/spring.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D
+
+func _on_spring_body_entered(body):
+	body.velocity.y = body.JUMPFORCE * 1.5
+	$AnimatedSprite.play("ON")
+	$Timer.start()
+
+func _on_Timer_timeout():
+	$AnimatedSprite.play("OFF")
