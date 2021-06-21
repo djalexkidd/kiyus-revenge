d12985003b5588367f896c6fb8f44f75746d8f27
djalexkidd <alexpyr@outlook.fr>
2021-06-21 20:50:16 +0200

    Le level design c'est dur

---
+++ scripts/levels/Level5.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _on_block_tilemap_trap_trigger(): #Active le piège tilemap
+	$"Tiles Solid".hide() #Cache la tilemap
+	$"Tiles Solid".set_collision_layer_bit(1,false) #Et désactive les collisions
+	$"Tiles Solid".set_collision_mask_bit(0,false) #Sinon c'est pas drôle
