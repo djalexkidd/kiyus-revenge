b239fe0cae800eed31d852e8ef8fe0baffd96c56
djalexkidd <alexpyr@outlook.fr>
2021-08-30 22:52:11 +0200

    Correction de la caméra et des plateformes

--- scripts/props/Board.gd
+++ scripts/props/Board.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 var speed = 100 #Vitesse
 var velocity = Vector2()
 export var directiony = -1 #-1 = Direction vers le haut, 1 = Direction vers le bas
-export var directionx = -1 #-1 = Direction vers la gauche, 1 = Direction vers la droite
+export var directionx = 0 #-1 = Direction vers la gauche, 1 = Direction vers la droite
 
 func _physics_process(delta):
 	velocity.y = speed * directiony
