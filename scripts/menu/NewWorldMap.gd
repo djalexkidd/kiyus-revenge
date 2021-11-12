404488d515799760c41a22a472ef58d75d02b966
djalexkidd <alexpyr@outlook.fr>
2021-11-12 23:10:22 +0100

    yeah mah boi

--- scripts/menu/NewWorldMap.gd
+++ scripts/menu/NewWorldMap.gd
@@ -5,16 +5,40 @@ func _on_Level_body_exited(body):
 	$AntiBug.start()
 
 func _on_Level1_body_entered(body):
-	Global.current_level = 1
+	match Global.current_world:
+		1:
+			Global.current_level = 1 #Change le niveau actuel vers le niveau 1-1
+		2:
+			Global.current_level = 5 #Change le niveau actuel vers le niveau 2-1
 
 func _on_Level2_body_entered(body):
-	Global.current_level = 2
+	match Global.current_world:
+		1:
+			Global.current_level = 2 #Change le niveau actuel vers le niveau 1-2
+		2:
+			Global.current_level = 6 #Change le niveau actuel vers le niveau 2-2
 
 func _on_Level3_body_entered(body):
-	Global.current_level = 3
+	match Global.current_world:
+		1:
+			Global.current_level = 3 #Change le niveau actuel vers le niveau 1-3
+		2:
+			Global.current_level = 7 #Change le niveau actuel vers le niveau 2-3
 
 func _on_Level4_body_entered(body):
-	Global.current_level = 4
+	match Global.current_world:
+		1:
+			Global.current_level = 4 #Change le niveau actuel vers le niveau 1-4
+		2:
+			Global.current_level = 8 #Change le niveau actuel vers le niveau 2-4
 
 func _on_AntiBug_timeout():
 	Global.current_level = 0
+
+func _on_World1_body_entered(body):
+	Global.current_world = 1
+	$HUD/Bar/CurrentWorld.text = "WORLDMAP_BIGTITLE"
+
+func _on_World2_body_entered(body):
+	Global.current_world = 2
+	$HUD/Bar/CurrentWorld.text = "WORLDMAP_BIGTITLE2"
