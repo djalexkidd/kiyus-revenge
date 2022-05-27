8f6cd469e608311a651c79a2274f2f34ed1f7fe7
djalexkidd <alexpyr@outlook.fr>
2022-05-27 15:33:43 +0200

    Bienvenue en 2022 mdr

--- scripts/levels/Level7.gd
+++ scripts/levels/Level7.gd
@@ -6,10 +6,11 @@ var camera_path = "res://scenes/levels/cameras/Camera7.tscn"
 func _ready():
 	var player_character = load(player_character_path).instance()
 	var camera = load(camera_path).instance()
-	player_character.position = Vector2(-88, 476)
+	player_character.position = Vector2(-250, 476)
 	add_child(player_character)
 	$Kiyu.add_child(camera)
 	$fallzone.connect("body_entered", $Kiyu, "_on_fallzone_body_entered")
 
 func _physics_process(delta):
 	$Kiyu/Camera2D.limit_left += 2
+	$Kiyu/Camera2D.limit_right += 2
