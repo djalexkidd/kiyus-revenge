1b49301fae6e0c26282449204c2bc8e41bef6596
djalexkidd <alexpyr@outlook.fr>
2023-04-28 16:15:33 +0200

    pas de bouton quitter quand c'est sur le web

--- scripts/menu/ReturnMenu.gd
+++ scripts/menu/ReturnMenu.gd
@@ -1,5 +1,9 @@
 extends Button
 
+func _ready():
+	if OS.get_name() == "HTML5":
+		queue_free()
+
 func _on_ReturnToMenu_pressed():
 	Global.coins = 0
 	get_tree().change_scene("res://scenes/menu/TitleMenu.tscn")
