9a04dcb0dae9d62fa3389b5b3f50b11c83e3e1fc
djalexkidd <alexpyr@outlook.fr>
2026-06-11 20:00:00 +0200

    Niveau 3-3

--- scripts/characters/PaulokFinalBoss.gd
+++ scripts/characters/PaulokFinalBoss.gd
@@ -14,7 +14,8 @@ func _physics_process(delta):
 	velocity.x += speed
 	velocity.y += gravity
 	
-	velocity = move_and_slide(velocity,Vector2.UP)
+	if health > 0:
+		velocity = move_and_slide(velocity,Vector2.UP)
 
 func _on_boss_trigger_body_entered(body):
 	gravity = 20
@@ -31,15 +32,12 @@ func _on_Hitbox_body_entered(body):
 		body.ouch(position.x)
 
 func _on_PaulokLeftSide_body_entered(body):
-	speed = 20
+	speed = 10
 	$AnimatedSprite.flip_h = false
 
 func _on_PaulokRightSide_body_entered(body):
-	speed = -20
+	speed = -10
 	$AnimatedSprite.flip_h = true
 
 func _on_PeppaPig_finished():
-	$Timer.start()
-
-func _on_Timer_timeout():
-	speed = -20
+	speed = -10
